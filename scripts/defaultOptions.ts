9aca3e69db4891a065966fcf057dfaf1c6267b4b
mawencan <1819293933@qq.com>
2022-03-26 16:53:37 +0800

    feat: 修正了配置

--- scripts/defaultOptions.ts
+++ scripts/defaultOptions.ts
@@ -1,29 +1,30 @@
 export type Type = 'sync' | 'diff'
-export interface ExecuteCommandOptions {
-  packagesPath: string
-  rootPackageIgnore: boolean
+export interface ExecuteCommandOption {
   mode: Type
-  version: {
-    message: string
-    mode: Type
-  }
-  publish: {
-    message: string
-    mode: Type
-    tag: string
-  }
+}
+export interface ExecuteCommandVersionOption extends
+  Partial<ExecuteCommandOption> {
+  message: string
+}
+export interface ExecuteCommandPublishOption extends
+  ExecuteCommandVersionOption {
+  tag: string
+}
+export interface ExecuteCommandOptions extends ExecuteCommandOption {
+  packagesPath: string
+  version: ExecuteCommandVersionOption
+  publish: ExecuteCommandPublishOption
 }
 export const defaultOptions: ExecuteCommandOptions = {
   packagesPath: 'packages/*',
-  rootPackageIgnore: true,
   mode: 'sync',
   version: {
+    mode: undefined,
     message: 'chore: version',
-    mode: 'sync',
   },
   publish: {
+    mode: undefined,
     message: 'chore: publish',
-    mode: 'sync',
     tag: '',
   },
 }
