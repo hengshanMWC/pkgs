49212ed4149aabfaf3bb69019de8e9d5821ce58a
mawencan <1819293933@qq.com>
2022-04-01 20:53:05 +0800

    fix: 修复init问题

--- src/cmd/init/index.ts
+++ src/cmd/init/index.ts
@@ -1,5 +1,34 @@
 import path from 'path'
 import fs from 'fs-extra'
-export async function cmdInit () {
-  await fs.copy(path.resolve(__dirname, './template'), './')
+export function cmdInit () {
+  const packagesName = 'packages'
+  const pkgsJsonName = 'pkgs.json'
+  const packageJsonName = 'package.json'
+  const dirPath = path.resolve(__dirname, './template')
+  const pkgsJson = path.resolve(dirPath, pkgsJsonName)
+  const packageJson = path.resolve(dirPath, packageJsonName)
+  return Promise.all([
+    createMkdir(packagesName),
+    createFile(pkgsJsonName, pkgsJson),
+    createFile(packageJsonName, packageJson),
+  ])
+}
+async function createMkdir (dirName: string) {
+  try {
+    const stat = await fs.stat(dirName)
+    if (!stat.isDirectory()) {
+      throw new Error()
+    }
+  }
+  catch {
+    await fs.mkdir(dirName)
+  }
+}
+async function createFile (fileName: string, filePath: string) {
+  try {
+    await fs.access(fileName)
+  }
+  catch {
+    await fs.copyFile(filePath, fileName)
+  }
 }
