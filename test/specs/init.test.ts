2559cd8f46bfbb0a6e75b87b1c11974af7d4d93b
mawencan <1819293933@qq.com>
2022-04-01 21:33:36 +0800

    test: 补充init的测试

--- test/specs/init.test.ts
+++ test/specs/init.test.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import fs from 'fs-extra'
 import { executeCommandInit } from '../../index'
 import { io } from '../__fixtures__'
@@ -19,14 +20,23 @@ describe(cmd, () => {
     const _path = await io.mkdtemp(prefix)
     process.chdir(_path)
     await executeCommandInit()
-    console.log(_path)
     const stat = await fs.stat(packagesName)
     expect(stat.isDirectory()).toBeTruthy()
     const pkgsJsonNameResult = await fs.access(pkgsJsonName)
     expect(pkgsJsonNameResult).toBeUndefined()
     const packageJsonNameResult = await fs.access(packageJsonName)
     expect(packageJsonNameResult).toBeUndefined()
+    const packagesFile = path.resolve(packagesName, '_path')
+    await fs.writeFile(packagesFile, _path, 'utf-8')
+    await fs.appendFile(pkgsJsonName, _path, 'utf-8')
+    await fs.appendFile(packageJsonName, _path, 'utf-8')
     // 不会覆盖
     await executeCommandInit()
+    const packagesFileData = await fs.readFile(packagesFile, 'utf-8')
+    const pkgsJsonData = await fs.readFile(pkgsJsonName, 'utf-8')
+    const packageJsonData = await fs.readFile(packageJsonName, 'utf-8')
+    expect(packagesFileData.trim()).toBe(_path)
+    expect(pkgsJsonData.includes(_path)).toBeTruthy()
+    expect(packageJsonData.includes(_path)).toBeTruthy()
   })
 })
