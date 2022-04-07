336e4979026a2fe01c040185315d1dc2a081bbf6
mawencan <1819293933@qq.com>
2022-04-07 23:12:14 +0800

    ci: 调整release

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,9 +1,11 @@
 import { execSync } from 'child_process'
 import colors from 'colors'
 import { executeCommand } from '../index'
-console.log(`${colors.cyan.bold('release: start')} 🏗`)
-execSync('npm run test', { stdio: 'inherit' })
-execSync('npm run build', { stdio: 'inherit' })
-await executeCommand('version')
-await executeCommand('publish')
+console.log(`${colors.cyan.bold('release: start')} 🏗`);
+(async function () {
+  execSync('npm run test', { stdio: 'inherit' })
+  execSync('npm run build', { stdio: 'inherit' })
+  await executeCommand('version')
+  await executeCommand('publish')
+})()
 console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
