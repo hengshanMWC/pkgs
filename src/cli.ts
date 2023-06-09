a18b22e6bd08cd22d30c3497c28c298eaf9e4b83
mawencan <1819293933@qq.com>
2023-06-10 00:14:44 +0800

    refactor: 使用PluginGroup做插件系统

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,11 @@
 import { program } from 'commander'
+import { PluginGroup } from 'plugin-group'
 import pkg from '../package.json'
 import { cliVersion, cliSuccess } from './utils/tips'
-import { PluginStore } from './lib/plugin'
 import { Context } from './lib/context'
 import type { PluginData } from './defaultOptions'
 export async function cliMain (argv: NodeJS.Process['argv']) {
-  const pluginStore = new PluginStore<PluginData>()
+  const pluginStore = new PluginGroup<PluginData>()
   const config = await Context.assignConfig()
   pluginStore.use(...config.plugin)
   program
