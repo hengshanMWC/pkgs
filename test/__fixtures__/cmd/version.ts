3a3e17d1a0ae1226894270a9c49051c6cc023eec
mawencan <1819293933@qq.com>
2023-07-02 16:46:17 +0800

    test: version常量抽取

---
+++ test/__fixtures__/cmd/version.ts
@@ -0,0 +1,64 @@
+import type IPackageJson from '@ts-type/package-dts'
+import type { SimpleGit } from 'simple-git'
+import { getPackages, tagExpect } from '../commit'
+import { getNewestCommitId } from '../../../src/utils/git'
+import { handleCommand } from '../create-test-context'
+import { newSimpleGit } from '../instance'
+import { commandVersion } from '../../../src'
+const prefix = 'version-test'
+export const cmd = 'version'
+
+export async function tagCommit (packageJson: IPackageJson, version, git: SimpleGit) {
+  expect(packageJson.version).toBe(version)
+  const tagCommitId = await tagExpect(`${packageJson.name}@${version}`, git)
+  expect(tagCommitId).not.toBeUndefined()
+}
+async function getCommitMessage (git: SimpleGit) {
+  const newCommitId = await getNewestCommitId(git)
+  const gitMessage = await git.log([
+    '-1',
+    '--format=%B',
+    newCommitId,
+  ])
+  return gitMessage.latest?.hash
+}
+async function syncVersionDiff (version: string, git: SimpleGit) {
+  const tagCommitId = await tagExpect(`v${version}`, git)
+  const newCommitId = await getNewestCommitId(git)
+  expect(newCommitId.includes(tagCommitId)).toBeTruthy()
+}
+
+export async function syncTest (dir: string, arrFile: string[], newVersion: string) {
+  const message = 'chore: test'
+  const context = await handleCommand(dir, prefix)
+  const git = newSimpleGit(context.root)
+  process.chdir(context.root)
+  await commandVersion({
+    [cmd]: {
+      message: `${message} %s`,
+    },
+  }, git, newVersion)
+
+  // packages test
+  const arr = await getPackages(arrFile)
+  await syncVersionDiff(newVersion, git)
+  arr.forEach(({ version }) => expect(version).toBe(newVersion))
+  const gitMessage = await getCommitMessage(git)
+  expect(gitMessage).toBe(`${message} v${newVersion}`)
+  return git
+}
+export async function diffTest (dir: string, arrFile: string[], newVersion: string) {
+  const context = await handleCommand(dir, prefix)
+  const git = newSimpleGit(context.root)
+  process.chdir(context.root)
+  await commandVersion({
+    mode: 'diff',
+  }, git, newVersion)
+  const packageJsonList = await getPackages(arrFile)
+  const tagList = packageJsonList.map(packageJson => tagCommit(packageJson, newVersion, git))
+  await tagList
+  return {
+    context,
+    git,
+  }
+}
--- test/specs/version.test.ts
+++ test/specs/version.test.ts
@@ -1,74 +1,11 @@
-import type { SimpleGit } from 'simple-git'
-import type { IPackageJson } from '@ts-type/package-dts'
 import { commandVersion } from '../../src/index'
-import { getPackages, tagExpect } from '../__fixtures__/commit'
+import { getPackages } from '../__fixtures__/commit'
 import {
-  getNewestCommitId,
-} from '../../src/utils/git'
-import type { SimpleGitTestContext } from '../__fixtures__'
-import {
-  handleCommand,
-  newSimpleGit,
   setUpFilesAdded,
 } from '../__fixtures__'
-import { Interdependence, addVersion, many, newVersion, quarantine } from '../__fixtures__/constant'
+import { Interdependence, ORIGINAL_CWD, addVersion, many, newVersion, quarantine } from '../__fixtures__/constant'
+import { cmd, diffTest, syncTest, tagCommit } from '../__fixtures__/cmd/version'
 
-const ORIGINAL_CWD = process.cwd()
-const cmd = 'version'
-let context: SimpleGitTestContext
-const prefix = 'version-test'
-
-async function tagCommit (packageJson: IPackageJson, version, git: SimpleGit) {
-  expect(packageJson.version).toBe(version)
-  const tagCommitId = await tagExpect(`${packageJson.name}@${version}`, git)
-  expect(tagCommitId).not.toBeUndefined()
-}
-async function getCommitMessage (git: SimpleGit) {
-  const newCommitId = await getNewestCommitId(git)
-  const gitMessage = await git.log([
-    '-1',
-    '--format=%B',
-    newCommitId,
-  ])
-  return gitMessage.latest?.hash
-}
-async function syncVersionDiff (version: string, git: SimpleGit) {
-  const tagCommitId = await tagExpect(`v${version}`, git)
-  const newCommitId = await getNewestCommitId(git)
-  expect(newCommitId.includes(tagCommitId)).toBeTruthy()
-}
-
-async function syncTest (dir: string, arrFile: string[], newVersion: string) {
-  const message = 'chore: test'
-  context = await handleCommand(dir, prefix)
-  const git = newSimpleGit(context.root)
-  process.chdir(context.root)
-  await commandVersion({
-    [cmd]: {
-      message: `${message} %s`,
-    },
-  }, git, newVersion)
-
-  // packages test
-  const arr = await getPackages(arrFile)
-  await syncVersionDiff(newVersion, git)
-  arr.forEach(({ version }) => expect(version).toBe(newVersion))
-  const gitMessage = await getCommitMessage(git)
-  expect(gitMessage).toBe(`${message} v${newVersion}`)
-  return git
-}
-async function diffTest (dir: string, arrFile: string[], newVersion: string) {
-  context = await handleCommand(dir, prefix)
-  const git = newSimpleGit(context.root)
-  process.chdir(context.root)
-  await commandVersion({
-    mode: 'diff',
-  }, git, newVersion)
-  const packageJsonList = await getPackages(arrFile)
-  const tagList = packageJsonList.map(packageJson => tagCommit(packageJson, newVersion, git))
-  await tagList
-  return git
-}
 afterEach(() => {
   // Many of the tests in this file change the CWD, so change it back after each test
   process.chdir(ORIGINAL_CWD)
@@ -79,7 +16,7 @@ describe(`${cmd}: ${quarantine}`, () => {
     await syncTest(quarantine, dirArr, newVersion)
   })
   test(`${quarantine}`, async () => {
-    const git = await diffTest(quarantine, dirArr, newVersion)
+    const { git, context } = await diffTest(quarantine, dirArr, newVersion)
 
     await setUpFilesAdded(context, ['packages/a/test'])
     await commandVersion({
@@ -96,7 +33,7 @@ describe(`${cmd}: ${many}`, () => {
     await syncTest(many, dirArr, newVersion)
   })
   test(`${many}`, async () => {
-    const git = await diffTest(many, dirArr, newVersion)
+    const { git, context } = await diffTest(many, dirArr, newVersion)
 
     await setUpFilesAdded(context, ['packages/a/test'])
     await commandVersion({
@@ -116,7 +53,7 @@ describe(`${cmd}: ${Interdependence}`, () => {
     await syncTest(Interdependence, dirArr, newVersion)
   })
   test(`${Interdependence}`, async () => {
-    const git = await diffTest(Interdependence, dirArr, newVersion)
+    const { git, context } = await diffTest(Interdependence, dirArr, newVersion)
 
     await setUpFilesAdded(context, ['packages/a/test'])
     await commandVersion({
@@ -126,5 +63,5 @@ describe(`${cmd}: ${Interdependence}`, () => {
     await tagCommit(aPackageJson, addVersion, git)
     await tagCommit(bPackageJson, newVersion, git)
     await tagCommit(cPackageJson, addVersion, git)
-  }, 10000000)
+  })
 })
