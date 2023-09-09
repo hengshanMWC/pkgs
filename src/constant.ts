efb6da601451572d8b29b51db06f2ebcb5428ffe
mawencan <1819293933@qq.com>
2023-09-09 12:06:50 +0800

    feat: 添加Agent枚举

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,5 @@
+import { Agent } from './command'
+
 export const RELY_KEYS = [
   'bundleDependencies',
   'bundledDependencies',
@@ -14,3 +16,5 @@ export const PACKAGES_PATH = ['packages/*']
 export const WARN_NOW_CHANGE = 'canceled: No new commit'
 export const WARN_NOW_VERSION = 'canceled: The version has not changed'
 export const WARN_NOW_RUN = 'There are no packages that need to run commands'
+
+export const DEFAULT_AGENT = Agent.PNPM
