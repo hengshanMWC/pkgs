d9d16e647fa1a1186e014bf02db464662cf23bf6
mawencan <1819293933@qq.com>
2023-09-25 19:15:53 +0800

    docs: 补充了介绍和安装文档

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,21 +1,69 @@
 import { defineConfig } from 'vitepress'
+import { repository } from '../../package.json'
+
+const loginPath = '../assets/images/logo.svg'
+const githubPath = repository.url.replace(/git\+/, '')
+
 export default defineConfig({
-  title: 'lerna-template',
-  description: 'lerna模板',
+  title: '@abmao/pkgs',
+  description: '一个简单的、既可以用于 Mono-repo 又可以用于 Multi-repo 的项目管理器。',
   base: '/',
+  head: [
+    ['link', { rel: 'icon', href: loginPath }]
+  ],
   themeConfig: {
-    siteTitle: 'lerna-template',
+    siteTitle: '@abmao/pkgs',
+    logo: loginPath,
+    socialLinks: [
+      { icon: 'github', link: githubPath },
+    ],
     sidebar: [
       {
-        text: '介绍',
+        text: '指南',
         collapsible: true,
         items: [
           {
-            text: 'demo',
+            text: '简介', // 做什么、为什么要用、思想
             link: '/',
+          },
+          {
+            text: '快速起步', // 依赖安装、模板下载、运行命令
+            link: '/use',
+          },
+          {
+            text: '主要功能',
+            link: '/features',
+          },
+        ],
+      },
+      {
+        text: 'API',
+        collapsible: true,
+        items: [
+          {
+            text: 'Context',
+            link: '/lib',
+          },
+          {
+            text: '命令',
+            link: '/command',
+          },
+          {
+            text: 'Cli',
+            link: '/cli',
+          },
+        ],
+      },
+      {
+        text: '配置',
+        collapsible: true,
+        items: [
+          {
+            text: '配置索引',
+            link: '/config',
           },
         ],
       },
     ],
-  },
+  }
 })
---
+++ docs/use.md
@@ -0,0 +1,81 @@
+# 安装
+
+## 全局安装
+
+::: code-group
+
+  ```bash [npm]
+  npm install @abmao/pkgs -g
+  ```
+  ```bash [yarn]
+  yarn add @abmao/pkgs -g
+  ```
+  ```bash [pnpm]
+  pnpm add @abmao/pkgs -g
+  ```
+
+:::
+
+### 使用
+```bash
+pkgs init // 创建项目
+pkgs version // 升级包
+pkgs publish // 发布包
+```
+
+## 项目安装
+
+::: code-group
+
+  ```bash [npm]
+  npm install @abmao/pkgs
+  ```
+  ```bash [yarn]
+  yarn add @abmao/pkgs
+  ```
+  ```bash [pnpm]
+  pnpm add @abmao/pkgs
+  ```
+
+:::
+
+### 使用
+在 package.json 的 scripts 定义命令
+```json
+{
+  "scripts": {
+    "release": "pkgs version && pkgs publish",
+  }
+}
+```
+
+代码调用
+```js
+import { commandVersion, commandPublish } from '@abmao/pkgs'
+(async function () {
+  await commandVersion()
+  await commandPublish()
+})()
+```
+
+# 模板
+安装 [@abmao/items](https://github.com/hengshanMWC/items)，一键创建项目体验
+
+::: code-group
+
+  ```bash [npm]
+  npm install @abmao/items -g
+  ```
+  ```bash [yarn]
+  yarn add @abmao/items -g
+  ```
+  ```bash [pnpm]
+  pnpm add @abmao/items -g
+  ```
+
+:::
+
+```bash
+items create [name]
+```
+<img src="./assets//images//items.png">
