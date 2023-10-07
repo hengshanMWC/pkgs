9d6dc9fb6d9408ba3b143b8a72a3ebde142e7852
mawencan <1819293933@qq.com>
2023-10-07 15:27:57 +0800

    chore: 用unbuild打包ts

--- scripts/utils/build.ts
+++ scripts/utils/build.ts
@@ -2,6 +2,5 @@ import { $ } from 'execa'
 import { createTemplate } from './template'
 export async function build () {
   await $({ stdio: 'inherit' })`npm run build:unbuild`
-  await $({ stdio: 'inherit' })`npm run tsc`
   await createTemplate()
 }
