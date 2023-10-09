30de89d50649caefc3434b06cd184742de3c923a
mawencan <1819293933@qq.com>
2023-10-09 22:41:16 +0800

    docs: 构建在线文档

---
+++ docs/dist/assets/config_index.md.41656c13.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,Q as l}from"./chunks/framework.bfdd83ff.js";const u=JSON.parse('{"title":"配置索引","description":"","frontmatter":{},"headers":[],"relativePath":"config/index.md","filePath":"config/index.md"}'),o={name:"config/index.md"},p=l("",57),e=[p];function t(r,c,i,y,B,F){return a(),n("div",null,e)}const d=s(o,[["render",t]]);export{u as __pageData,d as default};
--- docs/dist/assets/config_index.md.64df2f0b.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as n,Q as l}from"./chunks/framework.bfdd83ff.js";const u=JSON.parse('{"title":"配置索引","description":"","frontmatter":{},"headers":[],"relativePath":"config/index.md","filePath":"config/index.md"}'),o={name:"config/index.md"},p=l("",53),e=[p];function t(r,c,i,y,B,D){return a(),n("div",null,e)}const d=s(o,[["render",t]]);export{u as __pageData,d as default};
